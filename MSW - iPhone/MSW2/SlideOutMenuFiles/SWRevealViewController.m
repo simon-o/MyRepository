a9fe3ab01a278407b7388d043871c1c2eea1bafc
simon_o <antoine.simon@epitech.eu>
2016-03-15 23:33:04 +0000

    changement de nom msw2 -> MSW

--- MSW - iPhone/MSW2/SlideOutMenuFiles/SWRevealViewController.m
+++ MSW - iPhone/MSW2/SlideOutMenuFiles/SWRevealViewController.m
@@ -1176,7 +1176,7 @@ const int FrontViewPositionNone = 0xff;
     NSTimeInterval duration = _toggleAnimationDuration;
 
     // Velocity driven change:
-    if (fabsf(velocity) > _quickFlickVelocity)
+    if (fabs(velocity) > _quickFlickVelocity)
     {
         // we may need to set the drag position and to adjust the animation duration
         CGFloat journey = xLocation;
@@ -1194,7 +1194,7 @@ const int FrontViewPositionNone = 0xff;
             }
         }
         
-        duration = fabsf(journey/velocity);
+        duration = fabs(journey/velocity);
     }
     
     // Position driven change:
@@ -1251,7 +1251,7 @@ const int FrontViewPositionNone = 0xff;
     
     NSTimeInterval duration = animated?_toggleAnimationDuration:0.0;
     NSTimeInterval firstDuration = duration;
-    int initialPosDif = abs( _frontViewPosition - preReplacementPosition );
+    int initialPosDif = ( _frontViewPosition - preReplacementPosition );
     if ( initialPosDif == 1 ) firstDuration *= 0.8;
     else if ( initialPosDif == 0 ) firstDuration = 0;
     
